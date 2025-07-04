382bda8fa11471affc0d295b00ca539b7f0e3491
Giuseppe D'Anna <giuseppe.danna@truelayer.com>
2025-07-04 12:37:07 +0200

    update ownership and ci/cd

--- examples/versions.tf
+++ examples/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     slack = {
-      source  = "pablovarela/slack"
+      source  = "TrueLayer/slack"
       version = "~> 1.0"
     }
   }
--- scripts/install_plugin.sh
+++ scripts/install_plugin.sh
@@ -6,28 +6,22 @@
 
 set -e
 
-oss=( linux darwin )
-archs=( amd64 386 )
+os="$(uname | tr '[:upper:]' '[:lower:]')"
+arch="$(uname -m)"
 plugins_dir="${HOME}/.terraform.d/plugins"
 
 install_plugin() {
   plugin=$1
   version=0.0.1
   plugin_name=terraform-provider-$(basename "${plugin}")
-  plugin_location=$(command -v "${plugin_name}")
+  plugin_location="${GOBIN:-${GOPATH}/bin}/${plugin_name}"
   echo "Installing Terraform plugin ${plugin}..."
-  for os in "${oss[@]}"
-  do
-    for arch in "${archs[@]}"
-    do
-      file="${plugin_name}_v${version}-${os}-${arch}"
-      plugin_dst="${plugins_dir}/${plugin}/${version}/${os}_${arch}/${file}"
-      mkdir -p "$(dirname "${plugin_dst}")"
-      echo "location: ${plugin_location}"
-      cp "${plugin_location}" "${plugin_dst}"
-      echo "Copied to ${plugin_dst}"
-    done
-  done
+  file="${plugin_name}_v${version}-${os}-${arch}"
+  plugin_dst="${plugins_dir}/${plugin}/${version}/${os}_${arch}/${file}"
+  mkdir -p "$(dirname "${plugin_dst}")"
+  echo "location: ${plugin_location}"
+  cp "${plugin_location}" "${plugin_dst}"
+  echo "Copied to ${plugin_dst}"
 }
 
-install_plugin "terraform.local.com/pablovarela/slack"
+install_plugin "terraform.local.com/TrueLayer/slack"
