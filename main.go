545843516dcf05f4d15eb1e2340922803a1000f2
Giuseppe D'Anna <giuseppe.danna@truelayer.com>
2025-07-04 12:57:31 +0200

    DVOPS-1062 Update ownership and ci/cd (#2)
    * update ownership and ci/cd
    
    * fix codeql pipeline triggers
    
    * Update CODE_OF_CONDUCT.md
    
    Co-authored-by: Alessio Casco <5306425+AlessioCasco@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Alessio Casco <5306425+AlessioCasco@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"github.com/TrueLayer/terraform-provider-slack/slack"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/pablovarela/terraform-provider-slack/slack"
 )
 
 func main() {
